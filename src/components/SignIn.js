b809a93786c4861c6cd441ef0de2d28aca2b2c24
shuaibu-shehu <shuaibushehukalifa@gmail.com>
2023-09-21 15:30:02 +0530

    eye on and off

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,56 +1,68 @@
 import { useState } from 'react';
-import { Link,useNavigate } from 'react-router-dom';
-import "./styles/form.css"
+import { Link, useNavigate } from 'react-router-dom';
+import './styles/form.css';
 import SignUp from './SignUp';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase';
 
 function SignIn() {
-
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [showPassword, setShowPassword] = useState(false); // Track whether the password is visible or not
 
-  const navigate=useNavigate()
+  const navigate = useNavigate();
 
-  const login=async ()=>{
-    try{
-    const user =await signInWithEmailAndPassword(
-     auth,
-     email,
-     password
-    )
-    setEmail('');
-    setPassword('')
-    console.log(user);
-    console.log("user signed in");
-    navigate('/Gallery')
-    } catch(error){
-        setError('Use the correct credentials');
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(auth, email, password);
+      setEmail('');
+      setPassword('');
+      console.log(user);
+      console.log('user signed in');
+      navigate('/Gallery');
+    } catch (error) {
+      setError('Use the correct credentials');
     }
- }
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
     <div className='form'>
       <h2>Login</h2>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+      <div className='input-div'>
+        <label>Email</label>
+        <input
+          type='email'
+          placeholder='Email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+
+      <label>Password</label>
+
+      <div className='input-div'>
+        <input
+          type={showPassword ? 'text' : 'password'} // Toggle between 'text' and 'password'
+          placeholder='Password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div onClick={togglePasswordVisibility} className='eye-icon'>
+          <ion-icon name={showPassword ? 'eye-off' : 'eye'}></ion-icon>
+        </div>
+      </div>
       <button onClick={login}>Login</button>
       {error && <p className='error'>{error}</p>}
-      <p>Don't have an accout? <Link to='/SignUp'>Register</Link></p>
+      <p>
+        Don't have an account? <Link to='/SignUp'>Register</Link>
+      </p>
     </div>
   );
-  console.log(email);
 }
 
 export default SignIn;
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,55 +1,63 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import "./styles/form.css"
+import './styles/form.css';
 import SignIn from './SignIn';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase';
 
 function SignUp() {
-
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-const navigate = useNavigate()
+  const [showPassword, setShowPassword] = useState(false); // Track whether the password is visible or not
+  const navigate = useNavigate();
 
-  const register=async ()=>{
-    try{
-    const user =await createUserWithEmailAndPassword(
-     auth,
-     email,
-     password
-    )
-    navigate('/gallery');
-    setEmail('');
-    setPassword('')
- 
-    } catch(error){
-        setError('check your login credentials');
+  const register = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(auth, email, password);
+      navigate('/gallery');
+      setEmail('');
+      setPassword('');
+    } catch (error) {
+      setError('Check your login credentials');
     }
- }
+  };
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
     <div className='form'>
       <h2>Sign Up</h2>
+      <div className='input-div'>
+        <label>Email</label>
       <input
-        type="email"
-        placeholder="Email"
+        type='email'
+        placeholder='Email'
         value={email}
         onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+        />
+        </div>
+      <label>Password</label>
+      <div className='input-div'>
+        <input
+          type={showPassword ? 'text' : 'password'} // Toggle between 'text' and 'password'
+          placeholder='Password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div onClick={togglePasswordVisibility} className='eye-icon'>
+          <ion-icon name={showPassword ? 'eye-off' : 'eye'}></ion-icon>
+        </div>
+      </div>
       <button onClick={register}>SignUp</button>
       {error && <p className='error'>{error}</p>}
-      <p>Already have an Acoount have an accout? <Link to='/'>SignIn</Link></p>
+      <p>
+        Already have an account? <Link to='/'>SignIn</Link>
+      </p>
     </div>
   );
-
 }
 
 export default SignUp;
